75f9c1e02dce6a590df19b48040bb6846dc4e6b6
Megan Talbot <meganftalbot@gmail.com>
2017-01-06 10:43:08 -0700

    API test working, finishing test before refactor

--- app/controllers/best_buy_controller.rb
+++ app/controllers/best_buy_controller.rb
@@ -3,9 +3,7 @@ class BestBuyController < ApplicationController
   def index
     zip = params[:zip]
      response =
-     Faraday.get("https://api.bestbuy.com/v1/stores(area(80210,25))?format=json&show=longName,city,distance,phone,storeType&pageSize=15&apiKey=ENV["best_buy_api_key"]")
-
-      # Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=storeId,storeType,name&pageSize=15&apiKey=#{ENV['best_buy_api_key']}")
+     Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=longName,city,distance,phone,storeType&pageSize=15&apiKey=#{ENV["api_key"]}")
       @number = JSON.parse(response.body)["total"]
      @info = JSON.parse(response.body)["stores"]
   end
--- spec/features/user_can_search_for_best_buy_locations_by_zip_spec.rb
+++ spec/features/user_can_search_for_best_buy_locations_by_zip_spec.rb
@@ -4,23 +4,27 @@ describe "As a user, when I visit root path and enter a zip code into search bar
   scenario "I can see closest best buy locations" do
 
     visit root_path
-    within 'search' do
-      fill_in 'zip_code' with: "80202"
+save_and_open_page
+    within "#zipsearch" do
+    fill_in "search[zip]", with: "80202"
     end
-
-    expect(current_path).to eq('/search')
-
-
+    # expect(current_path).to eq('/search')
+    expect(page).to have_content("16 Total Stores")
+    # expect(page).to have_content("15")
+    expect(page).to have_content("Best Buy - Belmar")
+    expect(page).to have_content("Lakewood")
+    expect(page).to have_content("Lakewood")
+    expect(page).to have_content("5.28")
+    expect(page).to have_content("303-742-8039")
+    expect(page).to have_content("BigBox")
   end
 end
 
-As a user
-When I visit "/"
-And I fill in a search box with "80202" and click "search"
-Then my current path should be "/search" (ignoring params)
-And I should see stores within 25 miles of 80202
-And I should see a message that says "16 Total Stores"
-And I should see exactly 15 results
-And I should see the long name, city, distance, phone number and store type for each of the 15 results
-
-The name will be a link in the next story:
+# As a user
+# When I visit "/"
+# And I fill in a search box with "80202" and click "search"
+# Then my current path should be "/search" (ignoring params)
+# And I should see stores within 25 miles of 80202
+# And I should see a message that says "16 Total Stores"
+# And I should see exactly 15 results
+# And I should see the long name, city, distance, phone number and store type for each of the 15 results
