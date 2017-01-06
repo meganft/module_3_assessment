4600f6f2f77228104c401c8b8ccd7ce39a3b939e
Megan Talbot <meganftalbot@gmail.com>
2017-01-06 10:28:48 -0700

    Add best buy zip code search bar, /search route

--- app/controllers/best_buy_controller.rb
+++ app/controllers/best_buy_controller.rb
@@ -1,7 +1,12 @@
 class BestBuyController < ApplicationController
 
   def index
-     https://api.bestbuy.com/v1/stores(area(80210,25))?format=json&show=storeId,storeType,name&pageSize=15&apiKey=ENV['best_buy_api_key']
+    zip = params[:zip]
+     response =
+     Faraday.get("https://api.bestbuy.com/v1/stores(area(80210,25))?format=json&show=longName,city,distance,phone,storeType&pageSize=15&apiKey=ENV["best_buy_api_key"]")
 
+      # Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=storeId,storeType,name&pageSize=15&apiKey=#{ENV['best_buy_api_key']}")
+      @number = JSON.parse(response.body)["total"]
+     @info = JSON.parse(response.body)["stores"]
   end
 end
